e394aa03f6adea82ec48de5bcfcf340e55802eef
Juan Andres <jalagrange@gmail.com>
2010-07-26 12:59:02 +0200

    Ya lee el job name y guarda dentro de proyecto y software

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -2,13 +2,15 @@
 class JobController < ApplicationController
   
   def show
-    @group_counts =  Job.find(
-              :all,
-              :select => '`group`, COUNT(`group`) AS number_of_jobs',
-              :group  => '`group`'
-          )
-    
-   /job.grupo=Grupo.find_or_create_by_nombre(elementoxmlgruponombre)/
+    @group_counts =  Job.groups
+    @time_in_queue = Job.time_in_queue
+    @time_in_execution = Job.time_in_execution
+    @memory = Job.memory * 1024
+    @virtual_memory = Job.virtual_memory * 1024
+    @cput = Job.cput
+    @walltime = Job.walltime
+     
+      /job.grupo=Grupo.find_or_create_by_nombre(elementoxmlgruponombre)/
   end
   
   def index
@@ -18,8 +20,13 @@ class JobController < ApplicationController
   def datasave
     @jobs = Job.build_from_xml()
     for job in @jobs do
+      /PRESENILE_fsproc_NMP_214.sh/
+     @process = job.jobname.split('_')
+      job.proyect = @process[0]
+      job.process = @process[1]
       job.save
     end
+    
   end
   
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -14,42 +14,89 @@ class Job < ActiveRecord::Base
 
   JOB_DIR = File.join('data', 'jobs')
 
-    def self.groups()
-      Job.find(
-         :all, 
-         :select => 'count(*) count, group', 
-         :group => 'group')
+    def self.groups
+      find(
+         :all,
+         :select => '`group`, COUNT(`group`) AS number_of_jobs',
+         :group  => '`group`'
+         )
     end
 
+  def self.time_in_queue
+    sum(:start) - sum(:ctime)
+  end
+  
+  def self.time_in_execution
+    sum(:end) - sum(:start)
+  end
+  
+  def self.memory
+    sum(:memory)
+  end
+  
+  def self.virtual_memory
+    sum(:virtual_memory)
+  end
+  
+  def self.cput
+    @times = find(:all,
+                :select => 'cput')
+   @total_time =0
+    for time in @times do  
+      @total_time += time_to_seconds(time.cput)
+      end
+   @total_time  
+
+  end
+  
+  def self.walltime
+     @times = find(:all,
+                  :select => 'walltime')
+     @total_time =0
+      for time in @times do  
+        @total_time += time_to_seconds(time.walltime)
+        end
+     @total_time
+  end
+
+  def self.time_to_seconds(time)
+  time.to_i - 946684800
+  end
+
+
+  
   def self.build_from_xml()
     jobs = []
-    input_file = "#{JOB_DIR}/1.xml"
+    input_file = "#{JOB_DIR}/4.xml"
     doc = XML::Document.file(input_file) 
     doc.find('//execution_record').each do |node| 
-      jobs << Job.new(
-        :jobid => node.find('jobid').to_a.first.content,
-        :day => node.find('date/day').to_a.first.content,
-        :time => node.find('date/time').to_a.first.content,
-        :user => node.find('user').to_a.first.content,
-        :group => node.find('group').to_a.first.content,
-        :jobname => node.find('jobname').to_a.first.content,
-        :queue => node.find('queue').to_a.first.content,
-        :ctime => node.find('ctime').to_a.first.content,
-        :qtime => node.find('qtime').to_a.first.content,
-        :etime => node.find('etime').to_a.first.content,
-        :start => node.find('start').to_a.first.content,
-        :owner => node.find('owner').to_a.first.content,
-        :session => node.find('session').to_a.first.content,
-        :end => node.find('end').to_a.first.content,
-  	    :exit_status => node.find('exit_status').to_a.first.content,
-  	    :node_name => node.find('resources_used/exec_host/node/nodename').to_a.first.content,
-  	    :cpu => node.find('resources_used/exec_host/node/cpu').to_a.first.content,
-  	    :cput => node.find('resources_used/cput').to_a.first.content,
-  	    :memory => node.find('resources_used/mem/memory').to_a.first.content,
-  	    :virtual_memory => node.find('resources_used/vmem/memory').to_a.first.content,
-  	    :walltime => node.find('resources_used/walltime').to_a.first.content
+        if node.find('group').to_a.first.content == "neuro"
+        jobs << Job.new(
+          :jobid => node.find('jobid').to_a.first.content,
+          :day => node.find('date/day').to_a.first.content,
+          :time => node.find('date/time').to_a.first.content,
+          :user => node.find('user').to_a.first.content,
+          :group => node.find('group').to_a.first.content,
+          :jobname => node.find('jobname').to_a.first.content,
+          :queue => node.find('queue').to_a.first.content,
+          :ctime => node.find('ctime').to_a.first.content,
+          :qtime => node.find('qtime').to_a.first.content,
+          :etime => node.find('etime').to_a.first.content,
+          :start => node.find('start').to_a.first.content,
+          :owner => node.find('owner').to_a.first.content,
+          :session => node.find('session').to_a.first.content,
+          :end => node.find('end').to_a.first.content,
+    	    :exit_status => node.find('exit_status').to_a.first.content,
+    	    :node_name => node.find('resources_used/exec_host/node/nodename').to_a.first.content,
+    	    :cpu => node.find('resources_used/exec_host/node/cpu').to_a.first.content,
+    	    :cput => node.find('resources_used/cput').to_a.first.content,
+    	    :memory => node.find('resources_used/mem/memory').to_a.first.content,
+    	    :virtual_memory => node.find('resources_used/vmem/memory').to_a.first.content,
+    	    :walltime => node.find('resources_used/walltime').to_a.first.content
+
   	
-      )
+        )
+      end
     end
     jobs
   end
